2bbe1738259c767005dd046adbfaa6db0d084de4
Yesit24 <mariarosachamorro102@gmail.com>
2024-02-29 12:13:43 -0500

    simulador bancario

--- simulador bancario/CuentaAhorro.py
+++ simulador bancario/CuentaAhorro.py
@@ -2,23 +2,38 @@ class CuentaAhorro:
     """--------------------------
     #Atributos
     -----------------------------"""
-    saldoAhorro= 0
-    interesMensual= 0
+    saldoAhorro = ""
+    interesMensual = ""
     """--------------------------
     #Metodos
     -----------------------------"""
+
+    def ConsultarSaldo(self):
+        return self.saldo
 
     def SaldoAhorro(self):
         #Aqui va el codigo del metodo
         return self.saldoAhorro
     
-    def ConsignarValor(self,saldoAhorro):
+    def ConsignarValor(self,valor):
+        # #Forma 1
+        # self.saldo += monto
+        # # Forma 2
+        # self.saldo = self.saldo + monto
+        # # Forma 3
+        # total = self.saldo + monto
+        # self.saldo = total
+
         nsaldoAhorro = self.saldoAhorro + ""
         self.saldoAhorro = nsaldoAhorro
         return "Ingrese nuevo valor" + ""
-    def RetirarValor(self,saldoAhorro):
-        nsaldoAhorro = self.saldoAhorro - ""
-        self.saldoAhorro = nsaldoAhorro
-        return "Ingrese valor a retirar" + ""
     
+    def RetirarMonto(self, monto):
+        # #Forma 1
+        # self.saldo -= monto
+        # # Forma 2
+        # self.saldo = self.saldo - monto
+        # # Forma 3
+        total = self.saldo - monto
+        self.saldo = total
     
